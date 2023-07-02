abfe1379383c0b9d6843f90d7b26f03925f7a6f3
Gabriel Souza <gabrielss_@outlook.com>
2023-07-02 16:22:08 -0300

    refactor: create product handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/gsouza97/go-expert-api/configs"
-	"github.com/gsouza97/go-expert-api/internal/dto"
 	"github.com/gsouza97/go-expert-api/internal/entity"
 	"github.com/gsouza97/go-expert-api/internal/infra/database"
+	"github.com/gsouza97/go-expert-api/internal/infra/webserver/handlers"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -26,38 +25,8 @@ func main() {
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
 
 	productDB := database.NewProductDB(db)
-	productHandler := NewProductHandler(productDB)
+	productHandler := handlers.NewProductHandler(productDB)
 
 	http.HandleFunc("/products", productHandler.CreateProduct)
 	http.ListenAndServe(":8000", nil)
-}
-
-type ProductHandler struct {
-	ProductDB database.ProductDBInterface
-}
-
-func NewProductHandler(db database.ProductDBInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var product dto.CreateProductInput
-	err := json.NewDecoder(r.Body).Decode(&product)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	p, err := entity.NewProduct(product.Name, product.Price)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	err = h.ProductDB.CreateProduct(p)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
 }
---
+++ internal/infra/webserver/handlers/product_handlers.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gsouza97/go-expert-api/internal/dto"
+	"github.com/gsouza97/go-expert-api/internal/entity"
+	"github.com/gsouza97/go-expert-api/internal/infra/database"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductDBInterface
+}
+
+func NewProductHandler(db database.ProductDBInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.ProductDB.CreateProduct(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
